e1a2734347262dd9aca513074f2535cc738dc8e8
vav396 <vav396@mail.ru>
2025-08-19 15:48:55 +0300

    Added class Car, I am Main

--- src/Car.java
+++ src/Car.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
-
 public class Car {
+
+    String brand;
+    String model;
+    int year;
+
+    public Car(String brand, String model, int year) {
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+    }
+
+    public void start() {
+        System.out.println("Машина " + brand + " " + model + " заведена.");
+    }
+
+    public void info() {
+        System.out.println("Машина " + brand + " " + model + " " + year + " года выпуска.");
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,9 @@ public class Main {
     public static void main(String[] args) {
         Person person = new Person("John", 40);
         person.introduce();
+
+        Car myCar = new Car("Toyota", "Corolla", 2020);
+        myCar.start();
+        myCar.info();
     }
 }
