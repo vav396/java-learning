486944f83518fec45aa4f619107e4ccf51bc214c
vav396 <vav396@mail.ru>
2025-08-20 17:58:25 +0300

    задача 2: инкопсуляция

--- src/fundamentalsJava/Dog.java
+++ src/fundamentalsJava/Dog.java
@@ -1,19 +1,40 @@
 package fundamentalsJava;
 
 public class Dog {
-    String name;
-    int age;
+    private String name;
+    private int age;
 
     public Dog(String name, int age) {
         this.name = name;
         this.age = age;
     }
 
-    public void bark() {
-        System.out.println("Гав! Я " + name + "!");
+    public String getName() {
+        return name;
     }
 
-    public void info() {
-        System.out.println("Имя: " + name + ", Возраст: " + age);
+    public void setName(String name) {
+        this.name = name;
     }
-}
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if (age < 0) {
+            System.out.println("Возраст не может быть отрицательным!");
+            return;
+        } else {
+            this.age = age;
+        }
+    }
+
+        public void bark() {
+            System.out.println("Гав! Я " + name + "!");
+        }
+
+        public void info () {
+            System.out.println("Имя: " + name + ", Возраст: " + age);
+        }
+    }
